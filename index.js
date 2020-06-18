ae05dde8c864e9988bd2dba0ab33ad6191806e8c
Alissa Balge <alissa.balge@gmail.com>
2020-06-18 21:56:35 +0000

    Created findAvailableMoves function - it checks which moves are safe and won't cause a collision - first it checks that my snake won't collide with itself - then other snakes - and finally that my snake won't go out of bounds - moves which are deemed safe are added to the possibleMoves array

--- index.js
+++ index.js
@@ -32,10 +32,127 @@ function handleStart(request, response) {
   response.status(200).send('ok')
 }
 
+function findAvailableMoves(head, position, height, width, snakes) {
+  var freeSpaces = [];
+
+  function checkUp() {
+    var match = false;
+    //loops through own body to see if moving up will cause a collision
+    for(var square of position) {
+      //one is added to the head.y because that would be new positon of head if the snake moved up
+      if(square.x === head.x && square.y === (head.y + 1)) {
+        match = true;
+      }
+    }
+    //loop through each snake
+    for(var snake of snakes) {
+      //loop through each snakes position
+      for(var square of snake.body) {
+        if(square.x === head.x && square.y === (head.y + 1)) {
+          match = true;
+        }
+      }
+    }
+     
+    return match;
+  }
+
+  function checkDown() {
+    var match = false;
+    for(var square of position) {
+      if(square.x === head.x && square.y === (head.y - 1)) {
+        match = true;
+      }
+    }
+
+    for(var snake of snakes) {
+      for(var square of snake.body) {
+        if(square.x === head.x && square.y === (head.y - 1)) {
+          match = true;
+        }
+      }
+    }
+    
+    return match;
+  }
+
+  function checkRight() {
+    var match = false;
+    for(var square of position) {
+      if(square.x === (head.x + 1) && square.y === head.y) {
+        match = true;
+      }
+    }
+    for(var snake of snakes) {
+      for(var square of snake.body) {
+        if(square.x === (head.x + 1) && square.y === head.y) {
+          match = true;
+        }
+      } 
+    }
+    return match;
+  }
+
+  function checkLeft() {
+    var match = false;
+    for(var square of position) {
+      if(square.x === (head.x - 1) && square.y === head.y) {
+        match = true;
+      }
+    }
+    for (var snake of snakes) {
+      for(var square of snake.body) {
+        if(square.x === (head.x - 1) && square.y === head.y) {
+          match = true;
+        }
+      }
+    }
+    
+    return match;
+  }
+
+  if(!checkUp()) {
+    //checks if moving up would result in being out of bounds
+    if(head.y !== (height - 1)) {
+      // if not push up to the free spaces array
+      freeSpaces.push("up");
+    }
+  }
+
+  if(!checkDown()) {
+    if(head.y !== 0) {
+      freeSpaces.push("down");
+    }
+  }
+
+  if(!checkRight()) {
+    if(head.x !== (width - 1)) {
+      freeSpaces.push("right");
+    }
+  }
+
+  if(!checkLeft()) {
+    if(head.x !== 0) {
+      freeSpaces.push("left");
+    }
+  }
+  console.log(freeSpaces);
+  return freeSpaces;
+}
+
 function handleMove(request, response) {
   var gameData = request.body
 
-  var possibleMoves = ['up', 'down', 'left', 'right']
+  var head = gameData.you.head;
+  var position = gameData.you.body;
+  var height = gameData.board.height;
+  var width = gameData.board.width;
+  var food = gameData.board.food;
+  var snakes = gameData.board.snakes;
+  var health = gameData.you.health;
+
+  var possibleMoves = findAvailableMoves(head, position, height, width, snakes);
+  
   var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
 
   console.log('MOVE: ' + move)
